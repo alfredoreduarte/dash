31f78a6d85e449e0e28dc97e5c4fd4f4d1d8f64a
Alfredo Re <alfredoreduarte@gmail.com>
2018-09-05 12:35:31 -0400

    added redux devtools

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import ReactDOM from "react-dom"
 import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import todoApp from "./reducers"
 
-const store = createStore(todoApp, applyMiddleware(thunk))
+const store = createStore(todoApp, {}, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
 	<Provider store={store}>
